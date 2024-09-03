26e94d9529536433149c960f6ad876c4157cb5e4
ligaopeng123 <1170234932@qq.com>
2024-09-03 19:03:53 +0800

    feat: 支持低版本浏览器 兼容webview

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,23 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import Components from 'unplugin-vue-components/vite'
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
+import legacyPlugin from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/vue-three/',
   plugins: [
     vue(),
+    legacyPlugin(
+      {
+        targets:['chrome>=64', 'chromeAndroid>=64'],  // 需要兼容的目标列表，可以设置多个
+        additionalLegacyPolyfills:['regenerator-runtime/runtime'] // 面向IE11时需要此插件
+      }
+    ),
     vueJsx(),
     vueDevTools(),
     Components({
